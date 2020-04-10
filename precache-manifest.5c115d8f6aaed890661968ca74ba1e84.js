a9bc6a1503f2e09efd28338217520ae640bfd261
David Essien <davidessienshare@gmail.com>
2020-04-10 08:34:28 +0100

    Updates

---
+++ precache-manifest.5c115d8f6aaed890661968ca74ba1e84.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b920c3cf322e2938ac2245c39084ec0",
+    "url": "/covid-19-estimator/index.html"
+  },
+  {
+    "revision": "0a3861f90a1c3ca12bb0",
+    "url": "/covid-19-estimator/static/css/main.e5d85f59.chunk.css"
+  },
+  {
+    "revision": "a584d9d0156b9757429d",
+    "url": "/covid-19-estimator/static/js/2.0cf7a723.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-19-estimator/static/js/2.0cf7a723.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a3861f90a1c3ca12bb0",
+    "url": "/covid-19-estimator/static/js/main.4eafe0fb.chunk.js"
+  },
+  {
+    "revision": "e7b575129a82be69f698",
+    "url": "/covid-19-estimator/static/js/runtime-main.97713b8f.js"
+  },
+  {
+    "revision": "42e4010d8518a4cbc9c33a70721b9985",
+    "url": "/covid-19-estimator/static/media/sdgswheel.42e4010d.png"
+  }
+]);
